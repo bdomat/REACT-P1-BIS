b2c3021496dea6cd196492ade6fc9aabe032207c
Bertrand Domat <bertrand.domat@gmail.com>
2023-03-06 17:36:07 +0100

    react course 1/2

---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo></Logo>
+      <Navigation></Navigation>
+      <h1>A propos</h1>
+
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        nihil unde cumque illo vitae optio assumenda accusantium ullam adipisci.
+        Beatae fuga vitae quia pariatur natus! Corrupti nam fugit vel
+        reiciendis!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+        facere. Unde sunt fuga voluptatum corporis cumque labore mollitia, dolor
+        vitae fugit excepturi nemo? Amet quos facilis rem, voluptatem placeat
+        inventore.
+      </p>
+    </div>
+  );
+};
+
+export default About;
